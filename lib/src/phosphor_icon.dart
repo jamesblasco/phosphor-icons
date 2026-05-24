71a31a4923b28f34416e88862dbe9604a7108c01
Jaime Blasco <jaime@sparkli.ai>
2026-05-24 12:09:05 +0200

    fix: duotone requiring no tree shaking

--- lib/src/phosphor_icon.dart
+++ lib/src/phosphor_icon.dart
@@ -17,6 +17,7 @@ class PhosphorIcon extends Icon {
     TextDirection? textDirection,
     this.duotoneSecondaryOpacity = 0.20,
     this.duotoneSecondaryColor,
+    this.duotone = false,
   }) : super(
           icon,
           color: color,
@@ -33,34 +34,50 @@ class PhosphorIcon extends Icon {
 
   final double duotoneSecondaryOpacity;
   final Color? duotoneSecondaryColor;
+  final bool duotone;
 
   @override
   Widget build(BuildContext context) {
     final iconData = icon;
-    if (iconData?.fontFamily == 'PhosphorDuotone') {
-      final secondaryIcon = IconData(
-        iconData!.codePoint - 1,
-        fontFamily: iconData.fontFamily,
-        fontPackage: iconData.fontPackage,
-        matchTextDirection: iconData.matchTextDirection,
+    if (duotone && iconData?.fontFamily == 'PhosphorDuotone') {
+      final iconTheme = IconTheme.of(context);
+      final iconSize = size ?? iconTheme.size;
+      final iconColor = duotoneSecondaryColor ?? color ?? iconTheme.color;
+      final iconOpacity = iconTheme.opacity ?? 1.0;
+      Widget secondaryIcon = Text(
+        String.fromCharCode(iconData!.codePoint - 1),
+        overflow: TextOverflow.visible,
+        textDirection: textDirection,
+        style: TextStyle(
+          inherit: false,
+          color: iconColor,
+          fontFamily: iconData.fontFamily,
+          fontFamilyFallback: iconData.fontFamilyFallback,
+          fontPackage: iconData.fontPackage,
+          fontSize: iconSize,
+          height: 1.0,
+          shadows: shadows,
+        ),
       );
+
+      if (iconData.matchTextDirection) {
+        final textDirection = this.textDirection ?? Directionality.of(context);
+        if (textDirection == TextDirection.rtl) {
+          secondaryIcon = Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()..scale(-1.0, 1.0, 1.0),
+            child: secondaryIcon,
+          );
+        }
+      }
+
       return Stack(
         alignment: Alignment.center,
         children: [
-          Opacity(
-            opacity: duotoneSecondaryOpacity,
-            child: Icon(
-              secondaryIcon,
-              key: key,
-              size: size,
-              fill: fill,
-              weight: weight,
-              grade: grade,
-              opticalSize: opticalSize,
-              color: duotoneSecondaryColor ?? color,
-              shadows: shadows,
-              semanticLabel: semanticLabel,
-              textDirection: textDirection,
+          ExcludeSemantics(
+            child: Opacity(
+              opacity: duotoneSecondaryOpacity * iconOpacity,
+              child: secondaryIcon,
             ),
           ),
           super.build(context),
